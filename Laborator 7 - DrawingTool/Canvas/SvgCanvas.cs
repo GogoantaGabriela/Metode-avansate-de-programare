70dabb4f912e8c6c3f362073ae5ee7f53c15ef11
GogoantaGabriela <217717851+GogoantaGabriela@users.noreply.github.com>
2026-05-24 15:28:30 +0300

    Add files via upload

---
+++ Laborator 7 - DrawingTool/Canvas/SvgCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrawingTool.Interfaces;
+using global::DrawingTool.Interfaces;
+using System.Text;
+
+namespace DrawingTool.Canvas
+{
+ 
+    public class SvgCanvas : ICanvas
+    {
+        private readonly StringBuilder _svgContent = new();
+
+        public void DrawLine(double x1, double y1, double x2, double y2)
+            => _svgContent.AppendLine($"  <line x1='{x1}' y1='{y1}' x2='{x2}' y2='{y2}' stroke='black' />");
+
+        public void DrawCircle(double cx, double cy, double r)
+            => _svgContent.AppendLine($"  <circle cx='{cx}' cy='{cy}' r='{r}' fill='none' stroke='black' />");
+
+        public void DrawRect(double x, double y, double w, double h)
+            => _svgContent.AppendLine($"  <rect x='{x}' y='{y}' width='{w}' height='{h}' fill='none' stroke='black' />");
+
+        public void DrawEllipse(double cx, double cy, double rx, double ry)
+            => _svgContent.AppendLine($"  <ellipse cx='{cx}' cy='{cy}' rx='{rx}' ry='{ry}' fill='none' stroke='black' />");
+
+        public string GetSvg()
+        {
+            return $"<svg xmlns='http://www.w3.org/2000/svg' version='1.1'>\n{_svgContent}</svg>";
+        }
+    }
+}
---
+++ Laborator 7 - DrawingTool/Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using DrawingTool.Interfaces;
+using DrawingTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingTool.Shapes
+{
+    public class Rectangle : IShape
+    {
+        private double _x, _y, _width, _height;
+
+        public Rectangle(double x, double y, double w, double h)
+            => (_x, _y, _width, _height) = (x, y, w, h);
+
+        public void Draw(ICanvas canvas) => canvas.DrawRect(_x, _y, _width, _height);
+
+        public void Move(double dx, double dy) { _x += dx; _y += dy; }
+
+        public void Scale(double factor) { _width *= factor; _height *= factor; }
+
+        public BoundingBox GetBoundingBox()
+            => new BoundingBox(_x, _y, _x + _width, _y + _height);
+    }
+}
