8ceee06978aa43f8627d92c0876d47a51766f539
GogoantaGabriela <217717851+GogoantaGabriela@users.noreply.github.com>
2026-05-24 15:23:27 +0300

    Add files via upload

---
+++ Task Manager/TaskManager.Core/EmailNotifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager.TaskManager.Core
+{
+    public class EmailNotifier : ITaskNotifier
+    {
+        public void Notify(TaskItem task) => Console.WriteLine($"Email sent to user for task: {task.Title} is completed.");
+    }
+
+    public class ConsoleNotifier : ITaskNotifier
+    {
+        public void Notify(TaskItem task) => Console.WriteLine($"Task completed: {task.Title}");
+    }
+    public class FileLogNotifier : ITaskNotifier
+    {
+        public void Notify(TaskItem task) => File.AppendAllText("tasks.log", $"[{DateTime.Now}] Done: {task.Title}\n");
+    }
+
+    public class SlackNotifier : ITaskNotifier
+    {
+        public void Notify(TaskItem task) => Console.WriteLine($"Slack notification: {task.Title}");
+    }
+}
---
+++ Task Manager/TaskManager.Core/TaskService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager.TaskManager.Core
+{
+    public class TaskService : ITaskRepository
+    {
+        public enum NotificationType
+        {
+            Email,
+            Console,
+            FileLog,
+            Slack
+        }
+
+        private readonly ITaskRepository _repository;
+        private readonly TaskValidator _validator;
+        private readonly IReadOnlyDictionary<NotificationType, ITaskNotifier> _notifier;
+
+        public TaskService(ITaskRepository repository, TaskValidator validator, IReadOnlyDictionary<NotificationType, ITaskNotifier> notifier)
+        {
+            _repository = repository;
+            _validator = validator;
+            _notifier = notifier;
+        }
+
+        public void Add(TaskItem task)
+        {
+            _validator.Validate(task);
+            _repository.Add(task);
+            _notifier[0].Notify(task);
+        }
+
+        public void Delete(TaskItem task)
+        {
+            _repository.Delete(task);
+        }
+
+        public void Update(TaskItem task)
+        {
+            _repository.Update(task);
+        }
+
+        public TaskItem GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public IEnumerable<TaskItem> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
